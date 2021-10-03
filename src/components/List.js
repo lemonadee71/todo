0373614ccdd9352662a8a0181c57c32e2c1a12bd
Lemon <riegoshinandrei@gmail.com>
2021-10-03 17:14:10 +0800

    :construction: Add basic list creation

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,25 @@
 import { createHook, html } from 'poor-man-jsx';
+import Core from '../core';
+import { PROJECT } from '../core/actions';
 
 // * This is is-list and should update for every new task
-const List = (data) => {
-  const [current] = createHook({ todos: data.items });
+const List = (projectID, data) => {
+  const [current, revoke] = createHook({ todos: data.items });
 
   const addTodo = () => {
     const rand = Math.random().toString(36).slice(2);
     current.todos = [...current.todos, { id: rand, title: rand }];
+    console.log(current);
+  };
+
+  const deleteList = () => {
+    Core.event.emit(PROJECT.LISTS.REMOVE, { project: projectID, id: data.id });
   };
 
   return html`
-    <div>
+    <div id="${data.id}" ${{ '@unmount': revoke }}>
       <p>${data.name}</p>
+      <button ${{ onClick: deleteList }}>Delete</button>
       <button ${{ onClick: addTodo }}>Add todo</button>
       <ul
         is-list
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,15 +1,53 @@
-import { html } from 'poor-man-jsx';
+import { createHook, html } from 'poor-man-jsx';
 import List from '../components/List';
 import Core from '../core';
+import { PROJECT } from '../core/actions';
 
 // * This is is-list and should update for every new list
 const Project = ({ data: { id } }) => {
   const project = Core.main.getProject(`project-${id}`);
+  const [data] = createHook({ lists: project.lists.items });
+
+  const unsubscribe = Core.event.on(
+    PROJECT.LISTS.ALL,
+    () => {
+      data.lists = Core.main.getProject(project.id).lists.items;
+    },
+    { order: 'last' }
+  );
+
+  const createNewList = (e) => {
+    e.preventDefault();
+
+    const input = e.target.elements['new-list'];
+    Core.event.emit(PROJECT.LISTS.ADD, {
+      project: project.id,
+      name: input.value,
+    });
+
+    input.value = '';
+  };
 
   return html`
-    <div>
+    <div ${{ '@unmount': unsubscribe }}>
+      <form ${{ onSubmit: createNewList }}>
+        <input
+          type="text"
+          name="new-list"
+          id="new-list"
+          placeholder="Create new list"
+        />
+      </form>
       <h3>${project.name}</h3>
-      <div>${project.lists.items.map((list) => List(list))}</div>
+      <div
+        is-list
+        keystring="id"
+        ${{
+          $children: data.$lists((lists) =>
+            lists.map((list) => List(project.id, list))
+          ),
+        }}
+      ></div>
     </div>
   `;
 };
