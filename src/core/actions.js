6b8c8e3e5c1a7bd1935533f766e9f4ec15713b37
Lemon <riegoshinandrei@gmail.com>
2021-10-09 18:02:39 +0800

    :art: Update actions

--- src/core/actions.js
+++ src/core/actions.js
@@ -4,26 +4,25 @@ const UPDATE = 'update';
 const TRANSFER = 'transfer';
 const SYNC = 'sync';
 
-const createActions = (prefix) =>
-  [ADD, REMOVE, UPDATE, TRANSFER].reduce((obj, action) => {
+const createActions = (prefix) => {
+  const actions = [ADD, REMOVE, UPDATE, TRANSFER];
+  const final = actions.reduce((obj, action) => {
     obj[action.toUpperCase()] = `${prefix}.${action}`;
 
     return obj;
   }, {});
+  final.ALL = Object.values(final);
+
+  return final;
+};
 
 const TASK = createActions('task');
-TASK.ALL = Object.values(TASK);
 TASK.LABELS = createActions('task.labels');
-TASK.LABELS.ALL = Object.values(TASK.LABELS);
 TASK.SUBTASKS = createActions('task.subtasks');
-TASK.SUBTASKS.ALL = Object.values(TASK.SUBTASKS);
 
 const PROJECT = createActions('project');
-PROJECT.ALL = Object.values(PROJECT);
 PROJECT.LABELS = createActions('project.labels');
-PROJECT.LABELS.ALL = Object.values(PROJECT.LABELS);
 PROJECT.LISTS = createActions('project.lists');
-PROJECT.LISTS.ALL = Object.values(PROJECT.LISTS);
 
 PROJECT.SELECT = 'app.project.select';
 
