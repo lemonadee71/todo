c2b474d052080b0219f5f4fdbe7980a6986d51df
Lemon <riegoshinandrei@gmail.com>
2021-10-21 19:02:49 +0800

    :lipstick: Add basic styling to Task Item

--- src/components/Task.js
+++ src/components/Task.js
@@ -36,20 +36,23 @@ const Task = (data) => {
       ${{ $class: task.$completed((val) => (val ? 'task--done' : 'task')) }}
     >
       <input
+        class="task__checkbox"
         type="checkbox"
         name="mark-as-done"
         ${{ checked: data.completed, onChange: updateTask }}
       />
       <div class="task__body">
         <div class="task__labels"></div>
-        <p class="task__title">
-          <span class="task__name">${data.title}</span>
-          <span class="task__number">${data.numId}</span>
-        </p>
+        <div class="task__title">
+          <p class="task__name">${data.title}</p>
+          <span class="task__number">#${data.numId}</span>
+        </div>
         <div class="task__badges"></div>
       </div>
-      <button ${{ onClick: deleteTask }}>Delete</button>
-      <button ${{ onClick: editTask }}>Edit</button>
+      <div class="task__menu">
+        <button ${{ onClick: deleteTask }}>Delete</button>
+        <button ${{ onClick: editTask }}>Edit</button>
+      </div>
     </div>
   `;
 };
