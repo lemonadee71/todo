01dfd0afc3a0f740ac7e5ea7d7647235b7c16362
Lemon <riegoshinandrei@gmail.com>
2021-10-29 20:41:16 +0800

    :recycle: Make LabelPopover use useProject hook

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,16 +1,18 @@
 import { createHook } from 'poor-man-jsx';
 import Core from '../core';
 import { PROJECT, TASK } from '../core/actions';
+import { memoize } from './memo';
 
 // we rely on changes to original references
 // to be reflected here
-export const useProject = (projectId) => {
+export const useProject = memoize((projectId) => {
   const projectRef = Core.main.getProject(projectId);
   const [project] = createHook({
     // Add other properties if needed
     name: projectRef.name,
     id: projectRef.id,
     lists: projectRef.lists.items,
+    labels: projectRef.labels.items,
   });
 
   const unsubscribe = [
@@ -24,6 +26,7 @@ export const useProject = (projectId) => {
       ],
       () => {
         project.lists = projectRef.lists.items;
+        project.labels = projectRef.labels.items;
       },
       { order: 'last' }
     ),
@@ -32,7 +35,10 @@ export const useProject = (projectId) => {
     }),
   ];
 
-  const revoke = () => unsubscribe.forEach((cb) => cb());
+  const revoke = () => {
+    unsubscribe.forEach((cb) => cb());
+    useProject.__cache__.delete(JSON.stringify([projectId]));
+  };
 
   return [project, revoke];
-};
+});
--- src/utils/memo.js
+++ src/utils/memo.js
@@ -1,11 +1,15 @@
 export const memoize = (fn) => {
   const cache = new Map();
 
-  return (...args) => {
+  const memoized = (...args) => {
     const key = JSON.stringify(args);
     const result = cache.get(key) ?? fn(...args);
     cache.set(key, result);
 
     return result;
   };
+
+  memoized.__cache__ = cache;
+
+  return memoized;
 };
