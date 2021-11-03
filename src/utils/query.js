c0b4c9b82c2ef0f1c69649a4a9c8a9f39f39b992
Lemon <riegoshinandrei@gmail.com>
2021-11-03 21:01:03 +0800

    :recycle: Update query

--- src/utils/query.js
+++ src/utils/query.js
@@ -1,17 +1,29 @@
-function attr(name, value = null) {
-  return this(`[${name}${value ? `="${value}"` : ''}]`);
+function attr(name, value = null, context = document) {
+  return this(`[${name}${value ? `="${value}"` : ''}]`, context);
 }
 
-function data(name, value = null) {
-  return attr.call(this, `data-${name}`, value);
+function data(name, value = null, context = document) {
+  return attr.call(this, `data-${name}`, value, context);
 }
 
-function $(selector) {
-  return document.querySelector(selector);
+function $(selector, context = document) {
+  return context.querySelector(selector);
 }
 
-function $$(selector) {
-  return Array.from(document.querySelectorAll(selector));
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+function byId(id) {
+  return document.getElementById(id);
+}
+
+function byClassName(cls, context = document) {
+  return Array.from(context.getElementsByClassName(cls));
+}
+
+function byTagName(tag, context = document) {
+  return Array.from(context.getElementsByTagName(tag));
 }
 
 $.attr = attr;
@@ -19,6 +31,12 @@ $.data = data;
 
 $$.attr = attr;
 $$.data = data;
+
+$.by = attr.bind($$);
+
+$.by.id = byId;
+$.by.class = byClassName;
+$.by.tag = byTagName;
 
 $.all = $$;
 
