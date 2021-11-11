c5282a33696a9e7cbe0a00133500629552fd69f3
Lemon <riegoshinandrei@gmail.com>
2021-11-11 17:55:54 +0800

    :sparkles: Add fullcalendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,5 +1,29 @@
+import { Calendar as FullCalendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 import { html } from 'poor-man-jsx';
 
-const Calendar = () => html`<h3>This is my Calendar</h3>`;
+const Calendar = () => {
+  let self;
+
+  const init = function () {
+    self = new FullCalendar(this, {
+      plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+      initialView: 'dayGridMonth',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,listWeek',
+      },
+    });
+
+    self.render();
+  };
+
+  const destroy = () => self.destroy();
+
+  return html`<div ${{ onMount: init, onDestroy: destroy }}></div>`;
+};
 
 export default Calendar;
