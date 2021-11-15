682e3d4ed1d07240e1997e339cf7b2ccdec67dd0
Lemon <riegoshinandrei@gmail.com>
2021-11-15 21:04:57 +0800

    :recycle: Remove default delay for showToast

--- src/utils/showToast.js
+++ src/utils/showToast.js
@@ -4,20 +4,15 @@ import Core from '../core';
 export const showToast = (options) => {
   const toast = Toastify({
     ...options,
-    duration: 3000,
     gravity: 'bottom',
     position: 'left',
     stopOnFocus: false,
   });
-
   const currentToasts = [...Core.state.toasts];
 
   // show only 4 toasts at a time
   // ignores the set duration
-  if (currentToasts.length > 3) {
-    const toRemove = currentToasts.shift();
-    toRemove.hideToast();
-  }
+  if (currentToasts.length > 3) currentToasts.shift().hideToast();
 
   toast.showToast();
   currentToasts.push(toast);
