3b66ea1339c55dfe5717520ff72520562db2094f
Lemon <riegoshinandrei@gmail.com>
2021-11-15 21:07:54 +0800

    :art: Update ProjectLink

--- src/components/ProjectLink.js
+++ src/components/ProjectLink.js
@@ -5,30 +5,17 @@ import { useUndo } from '../utils/undo';
 
 const ProjectLink = (data) => {
   const deleteProject = useUndo({
-    selector: `#${data.id}`,
+    selector: `[data-id="${data.id}"]`,
     text: 'Project removed',
-    callback: () =>
-      Core.event.emit(PROJECT.REMOVE, {
-        project: data.id,
-      }),
+    callback: () => Core.event.emit(PROJECT.REMOVE, { project: data.id }),
   });
 
   return html`
     <li data-id="${data.id}">
-      <a
-        is="navigo-link"
-        href="${`/app/${data.link}`}"
-        title="Project | ${data.name}"
-      >
+      <a is="navigo-link" href="${`/app/${data.link}`}" title="${data.name}">
         {% ${data.name} %}
       </a>
-      <button
-        ${{
-          onClick: deleteProject,
-        }}
-      >
-        Delete
-      </button>
+      <button ${{ onClick: deleteProject }}>Delete</button>
     </li>
   `;
 };
