55ce1c7c71d7cf289d6a1a357ea0d283cc96b333
Lemon <riegoshinandrei@gmail.com>
2021-11-18 20:39:48 +0800

    :construction: Add toggling at once of tasks
    There's still a bug. Once all checkbox are checked, view-all should be checked too

--- src/components/Calendar/Sidebar.js
+++ src/components/Calendar/Sidebar.js
@@ -1,16 +1,36 @@
 import { html } from 'poor-man-jsx';
 import { useRoot } from '../../core/hooks';
+import { $, $$ } from '../../utils/query';
 
 const Sidebar = (calendar) => {
   const [data] = useRoot();
 
   const toggleProject = (e) => {
     calendar.self.toggleSchedules(e.target.value, !e.target.checked);
+
+    if (!e.target.checked) $.attr('name', 'view-all').checked = false;
+  };
+
+  const toggleAll = (e) => {
+    $$('input', $.data('name', 'user-projects')).forEach((input) => {
+      input.checked = e.target.checked;
+      toggleProject({ target: input });
+    });
   };
 
   return html`
     <div data-name="sidebar">
+      <label>
+        <input
+          name="view-all"
+          type="checkbox"
+          checked
+          ${{ onChange: toggleAll }}
+        />
+        View all
+      </label>
       <ul
+        data-name="user-projects"
         ${{
           $children: data.$projects.map(
             (project) =>
