25e5d79d6e7f5ef33e52c139c03dce06f0b1236e
Lemon <riegoshinandrei@gmail.com>
2021-12-07 13:13:44 +0800

    :recycle: Change behavior of 'login' btn when user is still logged in

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,13 +2,31 @@ import { html } from 'poor-man-jsx';
 import Core from '../core';
 import { PATHS } from '../core/constants';
 
-const Landing = () => html`
-  <button
-    ${{ onClick: () => Core.router.navigate(PATHS.login, { title: 'Login' }) }}
-  >
-    Login
-  </button>
-  <h1>This is my landing page</h1>
-`;
+const Landing = () => {
+  const loginBtn = html`
+    <button
+      ${{
+        onClick: () => Core.router.navigate(PATHS.login, { title: 'Login' }),
+      }}
+    >
+      Login
+    </button>
+  `;
+
+  const goBackBtn = html`
+    <button
+      ${{
+        onClick: () => Core.router.navigate(PATHS.app, { title: 'Overview' }),
+      }}
+    >
+      Open app
+    </button>
+  `;
+
+  return html`
+    ${Core.state.currentUser ? goBackBtn : loginBtn}
+    <h1>This is my landing page</h1>
+  `;
+};
 
 export default Landing;
