519adb471077c9fbe7be1cb7d80af1f42627be23
Lemon <riegoshinandrei@gmail.com>
2021-12-27 16:48:07 +0800

    :sparkles: Add isGuest util

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,6 +2,8 @@ import { signOut as signOutUser, getAuth } from 'firebase/auth';
 import Core from '../core';
 import { LOCAL_USER, PATHS } from '../core/constants';
 
+export const isGuest = (id = Core.state.currentUser) => id === LOCAL_USER;
+
 export const getUser = () => getAuth().currentUser;
 
 export const signIn = (id = LOCAL_USER) => {
