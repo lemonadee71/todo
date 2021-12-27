b413cbbb2ffaeb056bdf60657855ba834680212a
Lemon <riegoshinandrei@gmail.com>
2021-12-27 12:15:15 +0800

    :recycle: Change default length of id to 20

--- src/utils/id.js
+++ src/utils/id.js
@@ -1,6 +1,6 @@
 const randomString = (length) => Math.random().toString(36).substr(2, length);
 
-const uuid = (length = 12) => {
+const uuid = (length = 20) => {
   const lengthA = Math.floor(length / 2);
   const lengthB = length - lengthA;
 
