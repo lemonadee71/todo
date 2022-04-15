ee7c9be0491283a6f7e3fa2e5c6c86f3a6d319c2
Lemon <riegoshinandrei@gmail.com>
2022-04-15 18:20:18 +0800

    :lipstick: Add basic styling to Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,18 +18,34 @@ const Login = () => {
   }
 
   async function signInWithGoogle() {
-    signInFirebase(new GoogleAuthProvider());
+    await signInFirebase(new GoogleAuthProvider());
   }
 
   async function signInWithGithub() {
-    signInFirebase(new GithubAuthProvider());
+    await signInFirebase(new GithubAuthProvider());
   }
 
   return html`
-    <div>
-      <button onClick=${signInWithGoogle}>Sign in with Google</button>
-      <button onClick=${signInWithGithub}>Sign in with Github</button>
-      <button onClick=${() => signIn()}>Sign in as Guest</button>
+    <h1 class="font-sans font-bold text-center text-3xl mt-40 mb-10">Login</h1>
+    <div class="font-sans flex flex-col space-y-3 w-64 mx-auto">
+      <button
+        class="text-white text-xl rounded-lg py-2 drop-shadow-xl focus:ring focus:ring-sky-300 bg-sky-600 hover:bg-sky-700"
+        onClick=${signInWithGoogle}
+      >
+        Google
+      </button>
+      <button
+        class="text-white text-xl rounded-lg py-2 drop-shadow-xl focus:ring focus:ring-neutral-400 bg-neutral-700 hover:bg-neutral-800"
+        onClick=${signInWithGithub}
+      >
+        Github
+      </button>
+      <button
+        class="text-white text-xl rounded-lg py-2 drop-shadow-xl focus:ring focus:ring-emerald-300 bg-emerald-500 hover:bg-emerald-600"
+        onClick=${() => signIn()}
+      >
+        As Guest
+      </button>
     </div>
   `;
 };
