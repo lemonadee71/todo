302c844dbf69defa51ca441a2e1fe81f771f1da4
Lemon <riegoshinandrei@gmail.com>
2022-04-21 16:34:19 +0800

    :lipstick: Fix styling of Badge

--- src/components/Project/Badge.js
+++ src/components/Project/Badge.js
@@ -3,7 +3,7 @@ import { html } from 'poor-man-jsx';
 const Badge = (text, color, props = {}) =>
   html`<div
     is-text
-    class="inline-block font-sans text-sm text-white py-1 px-2 cursor-pointer"
+    class="inline-block font-sans text-xs text-white py-1 px-2 cursor-pointer rounded"
     style="background-color: ${color}"
     ${props}
   >
