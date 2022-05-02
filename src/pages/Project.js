73ff78a420e2016e0a62d195e0841eebeb7d9919
Lemon <riegoshinandrei@gmail.com>
2022-05-02 16:40:25 +0800

    :lipstick: Use icons instead for toggling between list/calendar views

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -136,10 +136,55 @@ const Project = ({ data: { id } }) => {
     >
       <h1 class="text-3xl font-extrabold mt-8 mb-6">${project.$name}</h1>
       <button
-        class="text-sm text-black bg-neutral-400 px-3 py-2 rounded"
+        class="px-3 py-2 rounded active:ring"
+        data-tooltip-text="Switch to ${state.$isListView((value) =>
+          value ? 'calendar' : 'list'
+        )} view"
         onClick=${switchView}
       >
-        ${state.$isListView((value) => (value ? 'List' : 'Calendar'))} view
+        ${state.$isListView((value) =>
+          value
+            ? render(html`<svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="stroke-gray-800"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1.75"
+                stroke="#000000"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <rect x="4" y="5" width="16" height="16" rx="2" />
+                <line x1="16" y1="3" x2="16" y2="7" />
+                <line x1="8" y1="3" x2="8" y2="7" />
+                <line x1="4" y1="11" x2="20" y2="11" />
+                <line x1="11" y1="15" x2="12" y2="15" />
+                <line x1="12" y1="15" x2="12" y2="18" />
+              </svg>`)
+            : render(html`<svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="stroke-gray-800"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1.75"
+                stroke="#000000"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <line x1="9" y1="6" x2="20" y2="6" />
+                <line x1="9" y1="12" x2="20" y2="12" />
+                <line x1="9" y1="18" x2="20" y2="18" />
+                <line x1="5" y1="6" x2="5" y2="6.01" />
+                <line x1="5" y1="12" x2="5" y2="12.01" />
+                <line x1="5" y1="18" x2="5" y2="18.01" />
+              </svg>`)
+        )}
       </button>
     </header>
 
