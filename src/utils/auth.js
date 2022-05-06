4d7ca3d1a4827812e2ed8c841d183840cfdde15a
Lemon <riegoshinandrei@gmail.com>
2022-05-06 12:38:12 +0800

    :sparkles: Add utils to get user info

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,6 +2,7 @@ import { signOut as signOutUser, getAuth } from 'firebase/auth';
 import { doc, getDoc, getFirestore } from 'firebase/firestore';
 import Core from '../core';
 import { LOCAL_USER, PATHS } from '../core/constants';
+import { getInitials } from './misc';
 
 export const isGuest = (id = Core.state.currentUser) => id === LOCAL_USER;
 
@@ -23,3 +24,14 @@ export const signOut = () => {
   Core.clearData();
   Core.router.redirect(PATHS.home, { title: 'To Do List' });
 };
+
+export const getProfilePicURL = () => {
+  const user = isGuest() ? { photoURL: '', displayName: 'Guest' } : getUser();
+  const url =
+    user.photoURL ||
+    `https://via.placeholder.com/60x60?text=${getInitials(user.displayName)}`;
+
+  return url;
+};
+
+export const getUserName = () => (isGuest() ? 'Guest' : getUser().displayName);
