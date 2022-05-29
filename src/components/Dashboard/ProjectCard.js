6a579d2f3af3d1a81c59cacf71a8924199cdebac
Lemon <riegoshinandrei@gmail.com>
2022-05-29 19:35:50 +0800

    :lipstick: Reduce shadow for ProjectCard

--- src/components/Dashboard/ProjectCard.js
+++ src/components/Dashboard/ProjectCard.js
@@ -15,7 +15,7 @@ const ProjectCard = (data, i) => {
   return html`
     <div
       key="${data.id}"
-      class="grid grid-rows-[1fr_2fr_1fr] rounded-xl shadow-xl bg-white dark:bg-[#272727] focus:ring group"
+      class="grid grid-rows-[1fr_2fr_1fr] rounded-xl shadow-md bg-white dark:bg-[#272727] focus:ring group"
       data-id="${data.id}"
       data-position="${i}"
       onDblClick=${() =>
