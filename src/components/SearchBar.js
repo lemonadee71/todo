a26d30092bc7c2d8c319b12ddb8af6e85f869f00
Lemon <riegoshinandrei@gmail.com>
2022-07-14 09:04:49 +0800

    :recycle: Fix show/hide behavior of search results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,17 @@ import { $ } from '../utils/query';
 import { matches } from '../utils/search';
 import SearchResult from './SearchResult';
 
+/**
+ * TODO: Copy behavior of MDN's search bar
+ *
+ * -[x] Search preview isn't hidden as long as focus is inside the component
+ * -[x] Ctrl + K focuses the search bar
+ * -[] Up/Down arrow navigates the search results
+ * -[] Clicking enter opens a modal that displays the results
+ * -[] If limit is reached in preview, clicking 'more' will open a modal
+ */
 const SearchBar = () => {
+  let self;
   const [state] = createHook({
     query: '',
     showResults: false,
@@ -42,9 +52,9 @@ const SearchBar = () => {
       .sort((a, b) => b.score - a.score); // sort in ascending
   };
 
-  const toggleFocus = (e) => {
-    const isFocused = e.target === document.activeElement;
-    state.showResults = isFocused && e.target.value.trim();
+  const toggleFocus = () => {
+    state.showResults =
+      self.contains(document.activeElement) && $('#search').value.trim();
   };
 
   const toggleSearchMode = () => {
@@ -52,8 +62,7 @@ const SearchBar = () => {
     updateResults(state.query);
   };
 
-  const clearInput = () => {
-    $('#search').value = '';
+  const clearState = () => {
     state.query = '';
     state.results = [];
     state.showResults = false;
@@ -62,16 +71,25 @@ const SearchBar = () => {
   const onChange = (e) => {
     state.query = e.target.value.trim();
     updateResults(state.query);
-    toggleFocus(e);
+    toggleFocus();
+  };
+
+  const onNavigate = (e) => {
+    if (e.key === 'Tab') toggleFocus();
   };
 
   return html`
     <div
       class="group relative col-span-2 xs:col-span-1 xs:col-start-2 xs:row-start-1 sm:w-3/4 sm:max-w-2xl md:w-1/2 md:ml-56"
+      onKeydown=${debounce(onNavigate, 50)}
+      onMount=${(e) => {
+        self = e.target;
+      }}
     >
       <form
         role="search"
         class="peer justify-self-stretch flex items-center gap-1 sm:gap-2 px-3 rounded-full bg-gray-200 dark:bg-gray-100 focus-within:bg-white focus-within:ring-2 focus-within:ring-sky-400 hover:shadow-md dark:hover:shadow-slate-500"
+        autocomplete="off"
       >
         <label for="search">
           <span class="sr-only"> Search tasks </span>
@@ -82,13 +100,14 @@ const SearchBar = () => {
           })}
         </label>
         <input
-          class="flex-1 py-0.5 text-black bg-inherit focus:outline-none"
+          class="flex-1 py-0.5 text-black bg-inherit focus:outline-none placeholder:text-sm"
           type="text"
           name="search"
           id="search"
+          placeholder="Quick search (Ctrl+K)"
           onFocus=${toggleFocus}
-          onBlur=${debounce(toggleFocus, 150)}
-          onInput=${debounce(onChange, 400)}
+          onBlur=${debounce(toggleFocus, 50)}
+          onInput=${debounce(onChange, 300)}
         />
         <button
           type="button"
@@ -103,7 +122,7 @@ const SearchBar = () => {
         <button
           type="reset"
           class="invisible group-focus-within:visible"
-          onClick=${clearInput}
+          onClick=${clearState}
         >
           ${CloseIcon({
             cls: 'stroke-gray-600 hover:stroke-red-500',
@@ -114,9 +133,10 @@ const SearchBar = () => {
         </button>
       </form>
       <div
-        class="absolute top-8 left-0 right-0 p-1 divide-y-2 divide-gray-200 rounded-b-lg drop-shadow-lg bg-white dark:bg-[#272727] dark:divide-[#272727] ${state.$showResults(
-          (value) => (value ? 'block' : 'hidden')
-        )} "
+        class="absolute top-8 left-0 right-0 p-1 divide-y-2 divide-gray-200 rounded-b-lg drop-shadow-lg bg-white dark:bg-[#272727] dark:divide-[#272727]"
+        style_display="${state.$showResults((value) =>
+          value ? 'block' : 'none'
+        )}"
       >
         ${state.$results(renderResults)}
       </div>
