818da23e082b620a299efe4129d03c25c79e7f35
Lemon <riegoshinandrei@gmail.com>
2022-07-15 13:35:06 +0800

    :wheelchair: Add role of log to toast

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -3,7 +3,7 @@ import { html, render } from 'poor-man-jsx';
 const Toast = ({ text, color = 'white', action = null }) =>
   render(html`
     <div class="font-sans flex flex-1 justify-between items-center mr-1">
-      <p class="text-sm" style="color: ${color};">${text}</p>
+      <p role="log" class="text-sm" style="color: ${color};">${text}</p>
       ${action
         ? html`<button
             class="px-2 py-1 rounded font-bold text-sm text-slate-200 bg-neutral-800 hover:bg-neutral-500 focus:ring"
